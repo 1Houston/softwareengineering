2a479360aba7c88921474ec9e835baf583027e9b
1Houston <147937694+1Houston@users.noreply.github.com>
2023-10-30 11:44:23 +0700

    Added ready-made files

---
+++ project.py
@@ -0,0 +1,17 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import torch
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-ru-en")
+from diffusers import AutoPipelineForText2Image
+pipe = AutoPipelineForText2Image.from_pretrained("kandinsky-community/kandinsky-2-2-decoder", torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+inp = str(input("Запрос: "))
+input_ids = tokenizer(inp, return_tensors='pt').input_ids
+outputs = model.generate(input_ids=input_ids)
+a = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+prompt = a
+negative_prompt = "low quality, bad quality"
+
+image = pipe(prompt=prompt, negative_prompt=negative_prompt, prior_guidance_scale =1.0, height=768, width=768).images[0]
+image.save("output.png")
